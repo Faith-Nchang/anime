0abd310d7ac3dc7a498a62279deeb32b4eef13dc
Faith-Nchang <fnchang@montgomerycollege.edu>
2024-10-17 00:47:59 -0400

    updated banned list

--- animal_gallery/src/Components/BannedList.jsx
+++ animal_gallery/src/Components/BannedList.jsx
@@ -2,6 +2,7 @@ const BannedList = ({ banned, remove }) => {
     return (
       <div className="banned-list">
         <h2>Banned List</h2>
+        <p>select an attribute to ban it</p>
         <ul>
           {banned.map((item, index) => (
             <li key={index}>
